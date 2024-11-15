dc40717a60747da063a25f8924b1acd545bb9d47
Junnygram <you@example.com>
2024-11-15 17:51:32 +0100

    ft: docker compose

--- README.md
+++ README.md
@@ -1,7 +1,42 @@
 # hello-k8s
 
-Initialize the project and make it run locally.
+### 1. Initialize the project and run it locally
 
 ![Screenshot 2024-11-15 at 13 45 28](https://github.com/user-attachments/assets/a53adbe3-3093-4cb1-bdc3-a46aa9cb06c6)
 
+- Set up the project by following the initialization steps.
+- Run the app locally to test the basic functionality.
 
+---
+
+### 2. Dockerize the application
+
+- Created a `Dockerfile`, `.dockerignore`, and `docker-compose.yml` configuration.
+
+To Dockerize the application, follow these steps:
+
+1. **Build and start the app** in detached mode using Docker Compose:
+
+```bash
+docker-compose up --build -d
+```
+
+2. **Tag the Docker image** with the appropriate repository name:
+
+```bash
+docker tag hello-k8s junny27/hello-k8s:latest
+```
+
+3. **Log in to Docker Hub**:
+
+```bash
+docker login
+```
+
+4. **Push the Docker image** to Docker Hub:
+
+```bash
+docker push junny27/hello-k8s:latest
+```
+
+Once the push is successful, your image will be available in the `junny27/hello-k8s` repository on Docker Hub.
--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import os from 'os';
 
 const app = express();
 const port = 3000;
+const imageName = process.env.APP_NAME || 'No image name'; // Default to 'No image name' if undefined
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
@@ -12,36 +13,43 @@ app.get('/', (req, res) => {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Styled Message</title>
+    <title>K8s Demo</title>
     <style>
       body {
-        font-family: Arial, sans-serif;
+        font-family: 'Arial', sans-serif;
         display: flex;
         justify-content: center;
         align-items: center;
         height: 100vh;
         margin: 0;
-        background-color: #f4f4f4;
-        color: #333;
+        background-color: #e9ecef;
+        color: #495057;
       }
       .message {
-        font-size: 24px;
-        font-weight: bold;
-        color: #2c3e50;
-        padding: 20px;
-        border: 2px solid #2c3e50;
-        border-radius: 8px;
-        background-color: #ecf0f1;
+        font-size: 28px;
+        font-weight: 600;
+        color: #343a40;
+        padding: 30px;
+        border: 2px solid #6c757d;
+        border-radius: 12px;
+        background-color: #f8f9fa;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        text-align: center;
+      }
+      .message span {
+        color: #007bff;
+        font-weight: 700;
       }
     </style>
   </head>
   <body>
     <div class="message">
-      Hello from ${os.hostname()}
+      Hello from container <span>${os.hostname()}</span><br>
+      from image <span>${imageName}</span>
     </div>
   </body>
   </html>
-`;
+  `;
   res.send(message);
 });
 
